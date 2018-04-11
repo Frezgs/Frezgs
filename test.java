ca7a79c8978205ebf14bc1054575b8cdd7266363
Frezgs <38268218+Frezgs@users.noreply.github.com>
2018-04-10 22:36:08 -0300

    tester

---
+++ test.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class test{
+	public static void main(String[] args)throws IOException{
+		int five = 5;
+		String message = "This is just a test";
+		System.out.println(message + five);
+	}
+}
