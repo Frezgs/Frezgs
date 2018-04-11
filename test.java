798d26935274ecaa3e4888abfb108a1d00240954
Frezgs <38268218+Frezgs@users.noreply.github.com>
2018-04-10 22:52:57 -0300

    Revert "tester"
    This reverts commit ca7a79c8978205ebf14bc1054575b8cdd7266363.

--- test.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-class test{
-	public static void main(String[] args)throws IOException{
-		int five = 5;
-		String message = "This is just a test";
-		System.out.println(message + five);
-	}
-}
